0992287201c0848a58bffdb00053315ec8958fdb
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2023-10-17 17:28:12 +0800

    ShiftF for Cellular Automaton
    Press it when you hate those messy code!

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>F"] = {
+      function()
+        vim.cmd(":CellularAutomaton make_it_rain")
+      end,
+      desc = "Bad codes :("
+    }
   },
   t = {
     -- setting a mapping to false will disable it
